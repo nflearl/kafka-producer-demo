24e05408789a80465825d67f968f32936061a214
earl.nolan <earl.nolan@wgu.edu>
2024-06-27 15:58:52 -0700

    Added generation of domain class from schema definition.

--- src/main/java/edu/wgu/domain/Producer.java
+++ src/main/java/edu/wgu/domain/Producer.java
@@ -1,5 +1,6 @@
 package edu.wgu.domain;
 
+import edu.wgu.ZZEarl;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,16 +9,16 @@ import java.util.UUID;
 @Component
 public class Producer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, ZZEarl> kafkaTemplate;
 
-    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+    public Producer(KafkaTemplate<String, ZZEarl> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
         send("private-earl-test", "Java Producer: " + System.currentTimeMillis());
     }
 
     @SuppressWarnings({"SameParameterValue"})
     private void send(String topic, String message) {
-        kafkaTemplate.send(topic, new String(UUID.randomUUID().toString()));
+        kafkaTemplate.send(topic, new ZZEarl(UUID.randomUUID().toString()));
     }
 
 //    private record String(String wguid) {}
