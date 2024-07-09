af4463f97e27ec8e5f46f9cc7a0f6993bf499ef4
earl.nolan <earl.nolan@wgu.edu>
2024-07-09 10:45:52 -0700

    Published two different types.

--- src/main/java/edu/wgu/domain/Producer.java
+++ src/main/java/edu/wgu/domain/Producer.java
@@ -1,5 +1,6 @@
 package edu.wgu.domain;
 
+import edu.wgu.Book;
 import edu.wgu.ZZEarl;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -9,9 +10,9 @@ import java.util.UUID;
 @Component
 public class Producer {
 
-    private final KafkaTemplate<String, ZZEarl> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public Producer(KafkaTemplate<String, ZZEarl> kafkaTemplate) {
+    public Producer(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
         send("private-earl-test", "Java Producer: " + System.currentTimeMillis());
     }
@@ -19,6 +20,7 @@ public class Producer {
     @SuppressWarnings({"SameParameterValue"})
     private void send(String topic, String message) {
         kafkaTemplate.send(topic, new ZZEarl(UUID.randomUUID().toString()));
+        kafkaTemplate.send(topic, new Book("Jungle Book", "isbn", 99));
     }
 
 //    private record String(String wguid) {}
