0f27fa5c8d6d779440a61ff1a116c0f884731ce4
earl.nolan <earl.nolan@wgu.edu>
2024-07-09 17:48:20 -0700

    Added an event to be filtered out be the Publisher.

--- src/main/java/edu/wgu/domain/Producer.java
+++ src/main/java/edu/wgu/domain/Producer.java
@@ -1,6 +1,7 @@
 package edu.wgu.domain;
 
 import edu.wgu.Book;
+import edu.wgu.Dog;
 import edu.wgu.ZZEarl;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,7 @@ public class Producer {
     private void send(String topic, String message) {
         kafkaTemplate.send(topic, new ZZEarl(UUID.randomUUID().toString()));
         kafkaTemplate.send(topic, new Book("Jungle Book", "isbn", 99));
+        kafkaTemplate.send(topic, new Dog("Spot", 14));
     }
 
 //    private record String(String wguid) {}
